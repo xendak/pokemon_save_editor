b2074021f5e087b912d9241a3929e0b73c81bca9
Rafael Grossi <rg.grossi@outlook.com>
2025-05-20 23:53:13 +0000

    meh

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,11 @@ const rl = @cImport({
     @cInclude("raylib.h");
 });
 
+const std = @import("std");
+const print = std.debug.print;
+
+const SaveData = struct {};
+
 pub fn main() anyerror!void {
     const screen_width = 800;
     const screen_height = 450;
@@ -16,6 +21,6 @@ pub fn main() anyerror!void {
 
         rl.ClearBackground(rl.WHITE);
 
-        rl.DrawText("TODO: Pokemon HGSS Save Editor", 190, 200, 20, rl.BLACK);
+        rl.DrawText("TODO: Pokemon HGSS Save Editor", 50, 50, 20, rl.BLACK);
     }
 }
